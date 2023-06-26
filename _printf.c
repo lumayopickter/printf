ab67041b07d7de2090d7b5610c9633c016f6c2a6
pickter lumayo <pickterlumayo@gmail.com>
2023-06-26 07:46:05 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,66 +1,82 @@
+#include "main.h"
 #include <unistd.h>
 #include <stdarg.h>
-#include "main.h"
+
+/**
+ * _putchar - Writes a character to stdout.
+ * @c: The character to be written.
+ *
+ * Return: On success, the number of characters written.
+ *         On error, -1 is returned and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * _puts - Writes a string to stdout.
+ * @str: The string to be written.
+ *
+ * Return: The number of characters written.
+ */
+int _puts(char *str)
+{
+	int i = 0;
+
+	if (str == NULL)
+		str = "(null)";
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	return (i);
+}
 
 /**
  * _printf - Produces output according to a format.
  * @format: A character string containing zero or more directives.
  *
- * Return: The number of characters printed (excluding the null byte used to
- *         end output to strings).
+ * Return: The number of characters printed (excluding the null byte).
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
 	int printed_chars = 0;
-	char ch;
-	char *str;
 
 	va_start(args, format);
 
-	while (format && *format)
+	if (format == NULL)
+		return (-1);
+
+	while (*format)
 	{
 		if (*format == '%')
 		{
 			format++;
-			switch (*format)
-			{
-				case 'c':
-					ch = va_arg(args, int);
-					write(1, &ch, 1);
-					printed_chars++;
-					break;
-				case 's':
-					str = va_arg(args, char *);
-					while (*str)
-					{
-						write(1, str, 1);
-						str++;
-						printed_chars++;
-					}
-					break;
-				case '%':
-					write(1, "%", 1);
-					printed_chars++;
-					break;
-				default:
-					write(1, "%", 1);
-					write(1, format, 1);
-					printed_chars += 2;
-					break;
-			}
+			if (*format == '\0')
+				return (-1);
+
+			if (*format == 'c')
+				printed_chars += _putchar(va_arg(args, int));
+			else if (*format == 's')
+				printed_chars += _puts(va_arg(args, char *));
+			else if (*format == '%')
+				printed_chars += _putchar('%');
+			else
+				printed_chars += _putchar('%') + _putchar(*format);
 		}
 		else
-		{
-			write(1, format, 1);
-			printed_chars++;
-		}
+			printed_chars += _putchar(*format);
 
 		format++;
 	}
 
 	va_end(args);
 
-	return printed_chars;
+	return (printed_chars);
 }
 
