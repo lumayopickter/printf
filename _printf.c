93dd24dc8e8922c22063fa747dbc8dcb33a24305
pickter lumayo <pickterlumayo@gmail.com>
2023-06-26 06:55:19 -0700

    Add _printf.c and main.h

--- _printf.c
+++ _printf.c
@@ -1,74 +1,66 @@
+#include <stdio.h>
 #include <stdarg.h>
-#include <unistd.h>
 #include "main.h"
 
 /**
- * _putchar - Writes a character to stdout
- * @c: The character to be written
+ * _printf - Prints output according to a format.
+ * @format: A character string containing zero or more directives.
  *
- * Return: On success, returns the number of characters written.
- * On error, returns -1.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
- * _printf - Produces output according to a format
- * @format: The format string
- *
- * Return: The number of characters printed (excluding the null byte)
+ * Return: The number of characters printed (excluding the null byte used to
+ *         end output to strings).
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int count = 0;
+    va_list args;
+    int printed_chars = 0;
+    char ch;
+    char *str;
 
-	va_start(args, format);
+    va_start(args, format);
 
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+            switch (*format)
+            {
+                case 'c':
+                    ch = (char)va_arg(args, int);
+                    putchar(ch);
+                    printed_chars++;
+                    break;
+                case 's':
+                    str = va_arg(args, char *);
+                    while (*str)
+                    {
+                        putchar(*str);
+                        str++;
+                        printed_chars++;
+                    }
+                    break;
+                case '%':
+                    putchar('%');
+                    printed_chars++;
+                    break;
+                default:
+                    putchar('%');
+                    putchar(*format);
+                    printed_chars += 2;
+                    break;
+            }
+        }
+        else
+        {
+            putchar(*format);
+            printed_chars++;
+        }
 
-			if (*format == '%')
-			{
-				count += _putchar('%');
-			}
-			else if (*format == 'c')
-			{
-				int ch = va_arg(args, int);
-				count += _putchar(ch);
-			}
-			else if (*format == 's')
-			{
-				char *str = va_arg(args, char *);
-				if (str == NULL)
-					str = "(null)";
-				while (*str)
-				{
-					count += _putchar(*str);
-					str++;
-				}
-			}
-			else
-			{
-				count += _putchar('%');
-				count += _putchar(*format);
-			}
-		}
-		else
-		{
-			count += _putchar(*format);
-		}
+        format++;
+    }
 
-		format++;
-	}
+    va_end(args);
 
-	va_end(args);
-
-	return count;
+    return printed_chars;
 }
 
