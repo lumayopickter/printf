eacfc94f8ee9365335fd04c42ba50ece7296134e
pickter lumayo <pickterlumayo@gmail.com>
2023-06-27 12:28:42 -0700

    functions

--- _printf.c
+++ _printf.c
@@ -15,6 +15,28 @@ int _putchar(char c)
 }
 
 /**
+ * _puts - Writes a string to stdout.
+ * @str: The string to be written.
+ *
+ * Return: The number of characters written.
+ */
+int _puts(char *str)
+{
+	int i = 0;
+
+	if (str == NULL)
+		str = "(null)";
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	return (i);
+}
+
+/**
  * _printf - Produces output according to a format.
  * @format: A character string containing zero or more directives.
  *
@@ -38,26 +60,43 @@ int _printf(const char *format, ...)
 			if (*format == '\0')
 				return (-1);
 
-			if (*format == 'c')
-				printed_chars += _putchar(va_arg(args, int));
-			else if (*format == 's')
+			switch (*format)
 			{
-				char *str = va_arg(args, char *);
-				if (str == NULL)
-					str = "(null)";
-				while (*str)
-				{
-					printed_chars += _putchar(*str);
-					str++;
-				}
+				case 'c':
+					printed_chars += _putchar(va_arg(args, int));
+					break;
+				case 's':
+					printed_chars += _puts(va_arg(args, char *));
+					break;
+				case '%':
+					printed_chars += _putchar('%');
+					break;
+				case 'd':
+				case 'i':
+					{
+						int num = va_arg(args, int);
+						printed_chars += _putchar('%');
+						printed_chars += _putchar(*format);
+						printed_chars += _puts(": [");
+						printed_chars += _putchar(num < 0 ? '-' : '0');
+						printed_chars += _putchar('0');
+						printed_chars += _putchar('0');
+						printed_chars += _putchar('0');
+						printed_chars += _putchar('0');
+						printed_chars += _putchar(num < 0 ? -(num % 10) + '0' : (num % 10) + '0');
+						printed_chars += _putchar(']');
+						break;
+					}
+				default:
+					printed_chars += _putchar('%') + _putchar(*format);
+					break;
 			}
-			else if (*format == '%')
-				printed_chars += _putchar('%');
-			else
-				printed_chars += _putchar('%') + _putchar(*format);
 		}
 		else
-			printed_chars += _putchar(*format);
+		{
+			_putchar(*format);
+			printed_chars++;
+		}
 
 		format++;
 	}
